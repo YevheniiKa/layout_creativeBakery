ade8918bccb81aa4924fbcdacc130c159d72e944
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2026-04-04 20:02:59 +0300

    added hero-section, and main sections, starting working on footer part for mobile phones

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,15 @@
 module.exports = {
-  extends: "@mate-academy/stylelint-config",
-  plugins: [
-    "stylelint-scss"
-  ],
-  rules: {}
+  extends: '@mate-academy/stylelint-config',
+  plugins: ['stylelint-scss'],
+  rules: {
+    'import-notation': null,
+    'order/properties-order': null,
+    'order/order': null,
+    'block-no-empty': null,
+    'rule-empty-line-before': null,
+    'scss/load-partial-extension': null,
+    'font-family-name-quotes': null,
+    'no-empty-source': null,
+    'declaration-empty-line-before': null,
+  },
 };
