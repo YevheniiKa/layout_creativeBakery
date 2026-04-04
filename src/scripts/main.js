4d651e0d0c0ce6e4d593954be86625b294b6551e
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2026-04-04 18:09:47 +0300

    added color vars, reset.scss file, and header with nav menu for mobile phones

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const burgerBtn = document.querySelector('.header__burger-btn');
+const mobileMenu = document.querySelector('.mobile-menu');
+const btn = document.querySelector('.header__burger-menu');
+
+burgerBtn.addEventListener('click', () => {
+  const isOpen = mobileMenu.classList.toggle('mobile-menu--open');
+
+  burgerBtn.setAttribute('aria-expanded', isOpen);
+  mobileMenu.setAttribute('aria-hidden', !isOpen);
+});
+
+btn.addEventListener('click', () => {
+  btn.classList.toggle('header__burger-menu--open');
+});
